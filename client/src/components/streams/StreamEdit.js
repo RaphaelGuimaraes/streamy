e31d59d80b07b98cf3cc57a2d25b6e0ef1264551
Raphael Guimarães <raphaguimaraes@gmail.com>
2020-07-05 20:49:17 -0300

    Stream CRUD, RTMP Server & JSON Server

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const StreamEdit = () => {
-	return <div>StreamEdit</div>;
+import { editStream, fetchStream } from "../../actions";
+import StreamForm from "./StreamForm";
+
+class StreamEdit extends React.Component {
+	componentDidMount() {
+		this.props.fetchStream(this.props.match.params.id);
+	}
+
+	onSubmit = (formValues) => {
+		this.props.editStream(this.props.match.params.id, formValues)
+	}
+
+	render() {
+		const { title, description } = this.props.stream;
+
+		if (!this.props.stream) {
+			return <div>Loading...</div>;
+		}
+
+		return (
+			<div>
+				<h3>Edit a Stream</h3>
+				<StreamForm onSubmit={this.onSubmit} initialValues={{title, description}}></StreamForm>
+			</div>
+		);
+	}
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+	return {
+		stream: state.streams[ownProps.match.params.id]
+	}
+};
+
+export default connect(mapStateToProps, {
+	editStream,
+	fetchStream
+})(StreamEdit);
